cbd2fd30ee7b10fd5964793baba746e28d3bac17
박은진 <81479466+parkjanwa@users.noreply.github.com>
2021-07-16 23:15:38 +0900

    Add files via upload

---
+++ C++ 공부/백준 2839 설탕배달.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    int N;
+    3 <= N <= 5000;
+
+    int N3 = 3;
+    int N5 = 5;
+
+    void cal(int a, int b){
+        
+    }
+
+    scanf("%d", N);
+
+}
+
+

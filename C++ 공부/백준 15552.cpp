af02b9b25e15c0c6b7682dd4a793a6eecfa6d6f0
박은진 <81479466+parkjanwa@users.noreply.github.com>
2021-08-02 11:00:33 +0900

    Add files via upload

---
+++ C++ 공부/백준 15552.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int a, b, c;
+
+    scanf("%d", &c);
+
+    for (int i = 0; i < c; i++){
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a+b);
+    }
+
+    return 0;
+
+}

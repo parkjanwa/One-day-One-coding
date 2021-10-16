e14966d1246f5bbcc9ee8128549eb4b35574c1ca
박은진 <81479466+parkjanwa@users.noreply.github.com>
2021-10-16 20:41:44 +0900

    Update 백준_2581.cpp

--- C++ 공부/백준_2581.cpp
+++ C++ 공부/백준_2581.cpp
@@ -3,23 +3,24 @@
 int main(){
     int M, N;
     int sum = 0;
+    int min, check;
 
-    scanf("%d", &M);
-    scanf("%d", &N);
+    scanf("%d %d", &M, &N);
 
     for (int i = M; i <= N; i++){
+        check = 0;
         if (i == 1) continue;
         for(int j = 2; j < i; j++){
-            if(i % j == 0) break;
-            if(i == j){
-                sum += j;
-                break;
-            } 
-
+            if(i % j == 0) check = 1;
         }
+        if(check == 0){
+                if(sum == 0){
+                    min = i;
+                }
+                sum += i;
+            } 
     }
-    printf("%d", sum);
+    if(sum == 0) printf("-1\n");
+    else printf("%d %d", sum, min);
     return 0;
 }
-
-//아직 미완성

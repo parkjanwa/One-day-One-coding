a233f62379ab3820ebfa18dc4bd9bbeeef281405
박은진 <81479466+parkjanwa@users.noreply.github.com>
2021-10-18 23:03:30 +0900

    Create 백준_11653.cpp

---
+++ C++ 공부/백준_11653.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cmath>
+
+int main() {
+
+    int N, i, j;
+
+    scanf("%d", &N);
+
+    for (i = 2; i <= sqrt(N); i++) {
+        while(N%i == 0) {
+            printf("%d\n", i);
+            N /= i;
+        }
+    }
+
+    if (N != 1) printf("%d\n", N);
+
+    return 0;
+}
